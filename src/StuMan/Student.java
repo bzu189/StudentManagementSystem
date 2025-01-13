1435edf65910510aa970e25db72af6a8ae20b1c5
Sarah <itsarahm10@gmail.com>
2025-01-13 10:56:35 +0545

    adding getter and setter in Student.java file

--- src/StuMan/Student.java
+++ src/StuMan/Student.java
@@ -1,10 +1,24 @@
 package StuMan;
+
 class StudentID{
     String name;
     int age;
     
     StudentID(String name, int age){
-        
+        this.name = name;
+        this.age = age;
+    } 
+    void setName(String name) {
+        this.name = name;
+    }
+    void setAge(int age) {
+        this.age = age;
+    }
+    String getName() {
+        return this.name;
+    }
+    int getAge() {
+        return this.age;
     }
 }
 public class Student {
